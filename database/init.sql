a047682cd4ea5dbc8d3de3fb5670aa8f5e55cd34
Max Nikulin <perseusxii@gmail.com>
2026-01-10 14:16:09 +0300

    Обновил init.sql

--- database/init.sql
+++ database/init.sql
@@ -7,9 +7,9 @@ USE betting_platform;
 -- Но можем добавить начальные данные
 
 -- Создание тестового администратора
-INSERT INTO users (email, password_hash, username, role, balance, created_at, updated_at) 
-VALUES 
-  ('admin@example.com', '$2b$10$YourHashedPassword', 'Admin', 'participant', 0, NOW(), NOW())
+INSERT INTO users (email, password, firstName, lastName, role, balance, createdAt, updatedAt, isAdmin)
+VALUES
+    ('admin@example.com', '$2b$10$.LLR3QY5TAtj7JBfTS/eEennKv3uxEEzBzXBFcEkKCYiSs6VzwkUO', 'Admin','Admin', 'fan', 0, NOW(), NOW(), 1)
 ON DUPLICATE KEY UPDATE id=id;
 
 -- Можно добавить больше seed данных если нужно
